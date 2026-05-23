02ef18d110bcfce11da74f973258cd1a48a8d768
iLemon <alex@lemongaming.ltd>
2026-05-23 20:15:03 +0100

    Fix error

--- ChunkBusterPlugin/src/main/java/codes/biscuit/chunkbuster/events/OtherEvents.java
+++ ChunkBusterPlugin/src/main/java/codes/biscuit/chunkbuster/events/OtherEvents.java
@@ -1,18 +1,14 @@
 package codes.biscuit.chunkbuster.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
 import codes.biscuit.chunkbuster.ChunkBuster;
 
-import java.util.regex.Pattern;
-
 public class OtherEvents implements Listener {
 
     private ChunkBuster main;
-    private int version = -1;
 
     public OtherEvents(ChunkBuster main) {
         this.main = main;
@@ -20,22 +16,17 @@ public class OtherEvents implements Listener {
 
     @EventHandler
     public void onWaterFlow(BlockFromToEvent e) {
-        if (version == -1) {
-            version = Integer.valueOf(Bukkit.getBukkitVersion().split(Pattern.quote("-"))[0].split(Pattern.quote("."))[1]);
-        }
-        if (version >= 13) { //Material names are different in 1.13+
-            if (e.getBlock().getType().equals(Material.WATER) || e.getBlock().getType().equals(Material.LAVA)) {
-                if (!main.getUtils().getWaterChunks().contains(e.getBlock().getChunk()) && main.getUtils().getWaterChunks().contains(e.getToBlock().getChunk())) {
-                    e.setCancelled(true);
-                }
-            }
-        } else {
-            if (e.getBlock().getType().equals(Material.WATER) || e.getBlock().getType().equals(Material.valueOf("STATIONARY_WATER"))
-                    || e.getBlock().getType().equals(Material.LAVA) || e.getBlock().getType().equals(Material.valueOf("STATIONARY_LAVA"))) {
-                if (!main.getUtils().getWaterChunks().contains(e.getBlock().getChunk()) && main.getUtils().getWaterChunks().contains(e.getToBlock().getChunk())) {
-                    e.setCancelled(true);
-                }
+        if (isLiquid(e.getBlock().getType())) {
+            if (!main.getUtils().getWaterChunks().contains(e.getBlock().getChunk()) && main.getUtils().getWaterChunks().contains(e.getToBlock().getChunk())) {
+                e.setCancelled(true);
             }
         }
+    }
+
+    private boolean isLiquid(Material material) {
+        return material == Material.WATER
+                || material == Material.LAVA
+                || material.name().equals("STATIONARY_WATER")
+                || material.name().equals("STATIONARY_LAVA");
     }
 }
